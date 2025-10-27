9d1a03c0ccb658d7c42d19de9fa63a74d1c78d3d
AbduHacker <199925619+AbduHacker@users.noreply.github.com>
2025-10-27 06:01:28 +0530

    Add script for clearing numbers

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ function showNumbers(buttons, output) {
     buttons.addEventListener("click", (e) => {
         target = e.target;
         if (target.classList.contains("blue-button")) output.value += target.textContent;
-    })
+    });
+}
+function clearScreen(buttons ,output) {
+    buttons.addEventListener("click", (e) => {
+        target = e.target;
+        if (target.id === "clear") {
+            let text = output.value;
+            output.value = text.slice(0, -1);
+        };
+    });
 }
 showNumbers(buttonsDiv, screen);
+clearScreen(buttonsDiv, screen);
